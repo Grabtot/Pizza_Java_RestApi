02b38942fc2725247fa052653a235d5f6c37131a
Grabtot <dneshotkin@gmail.com>
2023-03-09 20:04:57 +0200

    Init

---
+++ src/test/java/edu/itstep/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.itstep.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
